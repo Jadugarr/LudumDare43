49a9a3a76e4391d73db35c7f799de69537e3ec18
Robin Weiss <r0bzilla@gmx.net>
2018-12-01 20:25:08 +0100

    Added decals to bunny babies

--- Assets/Scripts/Bunnies/SmallBunnyBehaviour.cs
+++ Assets/Scripts/Bunnies/SmallBunnyBehaviour.cs
@@ -7,18 +7,33 @@ namespace Bunnies
         [SerializeField]
         private Sprite mashedBunny;
 
+        [SerializeField]
+        private GameObject decal;
+
         private EventManager _eventManager = EventManager.Instance;
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.tag == "Level" || other.gameObject.tag == "Deathplane" || (other.gameObject.tag == "SmallBunny" &&
-                                                    other.gameObject.GetComponent<Rigidbody2D>().bodyType ==
-                                                    RigidbodyType2D.Static))
+            switch (other.gameObject.tag)
             {
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-                GetComponent<SpriteRenderer>().sprite = mashedBunny;
-                _eventManager.FireEvent(EventTypes.BunnyStuck, null);
+                case "Level":
+                    Stick();
+                    GameObject decalObject = Instantiate(decal, this.transform);
+                    break;
+                case "SmallBunny":
+                    Stick();
+                    break;
+                case "Deathplane":
+                    Destroy(this);
+                    break;
             }
+        }
+
+        private void Stick()
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            GetComponent<SpriteRenderer>().sprite = mashedBunny;
+            _eventManager.FireEvent(EventTypes.BunnyStuck, null);
         }
     }
 }
