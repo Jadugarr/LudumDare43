008aa6612f6ca3243b717c3046677a63883d9dcf
Robin Weiss <r0bzilla@gmx.net>
2018-12-01 13:06:13 +0100

    Added baby bunny graphics

--- Assets/Scripts/Bunnies/SmallBunnyBehaviour.cs
+++ Assets/Scripts/Bunnies/SmallBunnyBehaviour.cs
@@ -4,8 +4,11 @@ namespace Bunnies
 {
     public class SmallBunnyBehaviour : MonoBehaviour
     {
+        [SerializeField]
+        private Sprite mashedBunny;
+
         private EventManager _eventManager = EventManager.Instance;
-        
+
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.gameObject.tag == "Level" || other.gameObject.tag == "Deathplane" || (other.gameObject.tag == "SmallBunny" &&
@@ -13,7 +16,7 @@ namespace Bunnies
                                                     RigidbodyType2D.Static))
             {
                 GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-                
+                GetComponent<SpriteRenderer>().sprite = mashedBunny;
                 _eventManager.FireEvent(EventTypes.BunnyStuck, null);
             }
         }
