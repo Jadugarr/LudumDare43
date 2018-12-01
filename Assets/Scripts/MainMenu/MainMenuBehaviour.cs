dda8dfdea48e618408d7d108772767aaaaf7833c
TheSemo <maleficarchyal@gmail.com>
2018-12-01 04:03:48 +0100

    added event manager and basic main menu

---
+++ Assets/Scripts/MainMenu/MainMenuBehaviour.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class MainMenuBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Button gameButton;
+        [SerializeField] private Button exitButton;
+
+        private void Start()
+        {
+            gameButton.onClick.AddListener(OnGameButtonClicked);
+            exitButton.onClick.AddListener(OnExitButtonClicked);
+        }
+
+        private void OnExitButtonClicked()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        }
+
+        private void OnGameButtonClicked()
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
