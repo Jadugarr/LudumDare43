a0da8a8732b2d90fdda6d827f8b8861e22512e4d
Robin Weiss <r0bzilla@gmx.net>
2018-12-01 12:19:25 +0100

    Added a template for a shaggable bunny

---
+++ Assets/Scripts/Bunnies/ShagMe.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShagMe : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerPlatformerController2D player;
+
+    [SerializeField]
+    private Transform playerShagPosition;
+
+    private Animator shagPartnerAnimator;
+
+    void Start()
+    {
+        this.shagPartnerAnimator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        // TODO check if player is close enough
+        if (Input.GetButtonDown("Fire3"))
+            attemptShagging();
+
+    }
+
+
+    public void attemptShagging()
+    {
+        shagPartnerAnimator.SetTrigger("startShag");
+    }
+
+
+    public void startShag()
+    {
+        player.transform.position = playerShagPosition.position;
+        player.gameObject.SetActive(false);
+        shagPartnerAnimator.SetBool("isSleepy", true);
+    }
+
+
+    public void finishShag()
+    {
+        player.gameObject.SetActive(true);
+        if (shagPartnerAnimator.transform.localScale.x == -1)
+        {
+            // TODO player must face left
+        }
+        else
+        {
+            // TODO player must face right
+        }
+    }
+}
