72262e2ac0975011fd5e3f8635d53cc03cb9550b
TheSemo <maleficarchyal@gmail.com>
2018-12-01 05:16:31 +0100

    better spawn without immediate recoil

--- Assets/Scripts/Bunnies/BunnyController.cs
+++ Assets/Scripts/Bunnies/BunnyController.cs
@@ -13,7 +13,7 @@ namespace Bunnies
             {
                 Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Vector2 direction = (mousePos - playerBunny.transform.position).normalized;
-                Vector3 newBunnyPos = new Vector3(playerBunny.transform.position.x + direction.x, playerBunny.transform.position.y + direction.y, 0);
+                Vector3 newBunnyPos = new Vector3(playerBunny.transform.position.x + direction.x + 0.5f, playerBunny.transform.position.y + direction.y + 0.5f, 0);
                 GameObject newBunny = GameObject.Instantiate(bunnyPrefab, newBunnyPos, bunnyPrefab.transform.rotation);
                 newBunny.GetComponent<Rigidbody2D>().AddForce(direction * 1000f);
             }
