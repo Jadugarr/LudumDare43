4537216aa9b282a93cbb230f7f8d769d4239e7d6
TheSemo <maleficarchyal@gmail.com>
2018-12-01 04:57:22 +0100

    Basic bunny spawn logic

---
+++ Assets/Scripts/Bunnies/BunnyController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Bunnies
+{
+    public class BunnyController : MonoBehaviour
+    {
+        [SerializeField] private GameObject bunnyPrefab;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 bunnyPos = new Vector3(mousePos.x, mousePos.y, 0);
+                GameObject newBunny = GameObject.Instantiate(bunnyPrefab, bunnyPos, bunnyPrefab.transform.rotation);
+                newBunny.GetComponent<Rigidbody2D>().AddForce(new Vector2(100f, 100f));
+            }
+        }
+    }
+}
