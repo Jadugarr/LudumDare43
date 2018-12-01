088ee25cf4735ae5817cb18081374b7947d44e2e
Robin Weiss <r0bzilla@gmx.net>
2018-12-01 18:42:20 +0100

    Added pixel perfect camera and background

---
+++ Assets/Scripts/environment/ParallaxBackground.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField]
+    private float xFactor = 1.0f;
+
+    [SerializeField]
+    private float yFactor = 1.0f;
+
+    [SerializeField]
+    private bool tiedToBottom = true;
+
+
+    private Camera camera;
+    private SpriteRenderer background;
+    private float maxY;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.background = GetComponent<SpriteRenderer>();
+        this.camera = background.transform.parent.parent.gameObject.GetComponent<Camera>();
+        this.maxY = camera.transform.localPosition.y;
+
+        //camera.aspect
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 pos = background.transform.localPosition;
+        pos.x = background.size.x / 4f - (camera.transform.position.x * xFactor) % (background.size.x / 2f);
+        //pos.y = -camera.transform.position.y * yFactor;
+
+        if (tiedToBottom)
+            pos.y = Mathf.Min(pos.y, maxY);
+        background.transform.localPosition = pos;
+
+        //GetComponent<Renderer>().material.mainTextureOffset = new Vector2(pos.x, 0f);
+    }
+}
