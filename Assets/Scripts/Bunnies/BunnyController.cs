bade7eb58b3861462a3bd64d5f987455c698c0e0
TheSemo <maleficarchyal@gmail.com>
2018-12-01 07:09:26 +0100

    Bunny spawning is slightly better

--- Assets/Scripts/Bunnies/BunnyController.cs
+++ Assets/Scripts/Bunnies/BunnyController.cs
@@ -8,17 +8,18 @@ namespace Bunnies
         [SerializeField] private GameObject playerBunny;
 
         private EventManager _eventManager = EventManager.Instance;
-        
+
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Vector2 direction = (mousePos - playerBunny.transform.position).normalized;
-                Vector3 newBunnyPos = new Vector3(playerBunny.transform.position.x + direction.x + 0.5f, playerBunny.transform.position.y + direction.y + 0.5f, 0);
+                Vector3 newBunnyPos = new Vector3(playerBunny.transform.position.x + direction.x + (1f * direction.x),
+                    playerBunny.transform.position.y + direction.y + (1f * direction.y), 0);
                 GameObject newBunny = GameObject.Instantiate(bunnyPrefab, newBunnyPos, bunnyPrefab.transform.rotation);
                 newBunny.GetComponent<Rigidbody2D>().AddForce(direction * 1000f);
-                
+
                 _eventManager.FireEvent(EventTypes.BunnySpawned, null);
             }
         }
