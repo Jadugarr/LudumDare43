41517ab5c664e83e5874a18f84256164f10a2729
Robin Weiss <r0bzilla@gmx.net>
2018-12-01 04:34:42 +0100

    Committed shit from Platformer 2D project

---
+++ Assets/Scripts/environment/CollisionLayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This Enum represents the Collision layer masks.
+ */
+public enum CollisionLayer : int
+{
+    Default = 1,
+    TransparentFx = 1 << 1,
+    IgnoreRaycast = 1 << 2,
+    Water = 1 << 4,
+    UI = 1 << 5,
+    Environment = 1 << 8,
+    EnvironmentChecker = 1 << 9,
+    Hitbox = 1 << 10
+}
